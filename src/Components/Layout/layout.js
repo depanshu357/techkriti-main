0db5df1f06190711c98c0ee49dccd3b9def183f3
chitwang <chitwang21@iitk.ac.in>
2022-12-17 20:24:05 +0530

    top and bottom links

--- src/Components/Layout/layout.js
+++ src/Components/Layout/layout.js
@@ -4,12 +4,7 @@ import Sidenav from './sidenav';
 const Layout = ({children}) => {
     return(
         <>
-        {/* <div style={{"border":"2px solid red"}}>
-            {/* <Toolbar/>
-            <Sides/>
-            <Backdrop/> 
-            
-    </div> */}
+        
         <Sidenav/>        
         <main>{children}</main>
         </>
