ac315352d237ef789b3d1c144b6775a65a016284
Depanshu <depanshusahu057@gmail.com>
2022-12-30 21:22:58 +0530

    added animation to merchandise page

--- src/Components/Merchandise/Merchandise.js
+++ src/Components/Merchandise/Merchandise.js
@@ -1,9 +1,12 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import CloseIcon from '@mui/icons-material/Close';
 import "./merchandise.css"
+import Collapse from '@mui/material/Collapse';
 
-const Merchandise = () => {
+
+const Merchandise = (props) => {
+    const [checked,setChecked] = useState(false)
     const handleMerchandise = (event) => {
         console.log(event.target);
         const display = document.getElementsByClassName("popUpDisplay");
@@ -11,13 +14,25 @@ const Merchandise = () => {
     
         // event.target.classList.add("mylink-active")
       }
-      const handleMerchandiseClose = (event) =>{
+
+      const TimedFunction = () =>{
         const display = document.getElementsByClassName("popUpDisplay");
         display[0].classList.add("MerchandiseClose");
+
+      }
+
+      const handleMerchandiseClose = (event) =>{
+        const display = document.getElementsByClassName("popUpDisplay");
+        props.setChecked(pre=>!pre);
+        display[0].classList.add("beforeMerchandiseClose");
+        setTimeout(TimedFunction,800)
+        display[0].classList.remove("smallPage");
         console.log(display[0]);
       }
   return (
+    <>
     <div className="popUpDisplay MerchandiseClose">
+    <Collapse in={props.checked} sx={{display:"flex",flexWrap:"wrap",maxWidth:"600px"}}>
           <div className="contentPopUpDisplay">
 
           <div className="top0fPopUp neonText">TECHKRITI MERCHANDISE<span><CloseIcon sx={{fontSize:"50px",cursor:"pointer",position:"absolute",right:"20px",top:"20px"}} onClick={handleMerchandiseClose} /></span></div>
@@ -42,7 +57,10 @@ const Merchandise = () => {
             </div>
           </div>
           </div>
+    </Collapse>
           </div>
+
+    </>
   )
 }
 
